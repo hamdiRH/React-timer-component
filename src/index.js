8f9e4730158947761ed549e54d5b7de91ce9f0b7
hamdiRH <hamdirahal@gmail.com>
2019-04-15 19:58:42 +0100

    timer component

---
+++ src/index.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeMs: 0,
+      btn: "start",
+      interval:null
+    };
+  }
+  start = () => {
+    if (this.state.interval && this.state.btn === "pause") {
+      clearInterval(this.state.interval);
+      this.setState({
+        interval: undefined,
+        btn: "start"
+      });
+    } else {
+      const interval = setInterval(() => {
+        this.setState({
+          timeMs: this.state.timeMs + 1000
+        });
+      }, 1000);
+      this.setState({
+        interval: interval,
+        btn: "pause"
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState.timeMs = 0;
+    if (!this.state.interval) {
+      this.setState({
+        timeMs: 0,
+        interval: undefined
+      });
+      return;
+    }
+    clearInterval(this.state.interval);
+    this.setState({
+      timeMs: 0,
+      interval: undefined
+    });
+  };
+  render() {
+    return (
+      <div className="center mt-5">
+        <Time ms={this.state.timeMs} />
+        <button
+          className="btn border border-danger"
+          type="button"
+          style={{height:'60px',borderRadius:'50%',width:'60px'}}
+          onClick={this.start}
+        >{this.state.btn==='start' ? <i class="far fa-play-circle" style={{fontSize:'25px'}} ></i> : <i class="far fa-pause-circle" style={{fontSize:'25px'}}></i>  }</button>
+        <button
+          className="btn border border-danger"
+          style={{height:'60px',borderRadius:'50%',width:'60px'}}
+          type="button"
+          value="Reset"
+          onClick={this.reset}
+        ><i class="fas fa-stop" style={{fontSize:'25px'}}></i></button>
+      </div>
+    );
+  }
+}
+const msToTime = ms => {
+  const msPerSecond = 1000;
+  const msPerMinute = msPerSecond * 60;
+  const msPerHour = msPerMinute * 60;
+
+  const hours = Math.floor(ms / msPerHour);
+  const hoursRest = ms % msPerHour;
+  const minutes = Math.floor(hoursRest / msPerMinute);
+  const minutesRest = hoursRest % msPerMinute;
+  const seconds = Math.floor(minutesRest / msPerSecond);
+  return (
+    String(hours).padStart(2, "0") +
+    ":" +
+    String(minutes).padStart(2, "0") +
+    ":" +
+    String(seconds).padStart(2, "0")
+  );
+};
+
+const Time = ({ ms }) => {
+  return (
+    <div className="time-container">
+      <div className="time-inner-container">
+        <div className="time-digits">{msToTime(ms)}</div>
+        <div className="time-text">
+          <div className="time-text-item">Hour</div>
+          <div className="time-text-item">Minute</div>
+          <div className="time-text-item">Second</div>
+        </div>
+      </div>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Timer />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
